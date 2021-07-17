751d036ee33bb34152bad0a1387499479559a344
TamHuynh <tamhuynh@mapmaker.asia>
2021-07-17 20:56:13 +0700

    Add timeline

--- js/profile.js
+++ js/profile.js
@@ -4,10 +4,29 @@ $(window).scroll(function() {
     var windowWidth = $(window).width();
 
     if (scrollTop > 1 && windowWidth > 767) {
-        asideLeft.addClass('sticky');
+        asideLeft.addClass("sticky");
         asideLeft.css("top", scrollTop);
     } else {
         asideLeft.removeClass('sticky');
         asideLeft.css("top", '');
     }
+});
+
+$(document).ready(function() {
+    function drawTimeline(timeline, from, to) {
+        var fromY = from.position().top + from.outerHeight() + parseInt(from.css('margin-top')) - 2
+        var toY = to.position().top + parseInt(to.css('margin-top'))
+
+        timeline.css("top", fromY).height(toY - fromY + 6)
+    }
+
+    function drawTimelines() {
+        drawTimeline($("#timeline1"), $("#dot1"), $("#dot2"))
+    }
+
+    $(window).on('resize', function() {
+        drawTimelines()
+    });
+
+    drawTimelines()
 });
